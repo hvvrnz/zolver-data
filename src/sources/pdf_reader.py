0b7524d5fc933925352155eb72ec3b80bc7f7f84
unknown <jenny021105@naver.com>
2025-12-25 01:20:26 +0900

    fix: update pdf_reader path

--- src/sources/pdf_reader.py
+++ src/sources/pdf_reader.py
@@ -2,10 +2,13 @@ import pandas as pd
 import requests
 import fitz  # PyMuPDF 라이브러리 / pdf 열기/읽기
 from PIL import Image
+from pathlib import Path
 
-# 추후 전 년도 적용 가능하도록 자동화 예정 일단 2021만
+BASE_DIR = Path(__file__).resolve().parents[2] #project root
+
 def pdf_to_text(year,page_num):
-    pdf = fitz.open(f"../data/pdfs/final/{year}.pdf")
+    pdf_path = BASE_DIR / "data" / "pdfs" / "final" / f"{year}.pdf"
+    pdf = fitz.open(pdf_path)
     page = pdf.load_page(page_num)
     text = page.get_text()
     pdf.close()
