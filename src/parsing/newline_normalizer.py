905cd0533a0515065566088704891a772dc0fb39
unknown <jenny021105@naver.com>
2025-12-25 01:19:42 +0900

    fix: update newline_normalizer.py

--- src/parsing/newline_normalizer.py
+++ src/parsing/newline_normalizer.py
@@ -1,8 +1,12 @@
 import re
+from sources.pdf_reader import pdf_to_text
 
-def newline_normalize(str):
-    rm_whitespace = re.sub(r'\s', ' ', str) # 공백문자
+def newline_normalize(raw_text):
+    rm_whitespace = re.sub(r'\s', ' ', raw_text) # 공백문자
     rm_privmsg_1 = re.sub(r'[\uE000-\uF8FF]', '', rm_whitespace) # 사적문자 (1)
     rm_privmsg_2 = re.sub(r'[\U000F0000-\U000FFFFD]', '', rm_privmsg_1) # 사적문자 (2)
 
     return rm_privmsg_2
+
+raw_text = pdf_to_text(2021, 5)
+print(newline_normalize(raw_text))
